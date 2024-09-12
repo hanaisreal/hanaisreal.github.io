a439fd722605d22d1612d0ed34d88606389c9bdc
Hana Oh <108045487+hanaisreal@users.noreply.github.com>
2024-09-12 14:33:48 +0900

    Modified App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,27 +1,15 @@
 // src/App.tsx
 import React from 'react';
 import { BrowserRouter as Router, Route, Routes, Link } from 'react-router-dom';
-import Home from './components/Home';
-import Projects from './components/Projects';
-import Works from './components/Works';
+import Home from './components/Portfolio';
 
 const App: React.FC = () => {
   return (
     <Router>
       <div className="app">
-        <nav>
-          <ul>
-            <li><Link to="/">Home</Link></li>
-            <li><Link to="/projects">Projects</Link></li>
-            <li><Link to="/works">Works</Link></li>
-
-          </ul>
-        </nav>
 
         <Routes>
           <Route path="/" element={<Home />} />
-          <Route path="/projects" element={<Projects />} />
-          <Route path="/works" element={<Works />} />
         </Routes>
 
         <footer>
