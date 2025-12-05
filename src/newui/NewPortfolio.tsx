0e582cabb20f15b152492d19ff920ee49e50eadd
Hana Oh <108045487+hanaisreal@users.noreply.github.com>
2025-12-05 14:35:54 +0900

    update bug fix

--- src/newui/NewPortfolio.tsx
+++ src/newui/NewPortfolio.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState } from 'react';
 import HeroSection from './HeroSection';
 import ResearchSection from './ResearchSection';
 import PublicationsSection from './PublicationsSection';
